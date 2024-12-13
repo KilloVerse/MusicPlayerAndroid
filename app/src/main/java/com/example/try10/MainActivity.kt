6a3495bc602c366fd64d5414d1d7473f1ddd16a5
Administrator <15brigada15@vk.com>
2024-12-13 10:48:49 +0300

    3 day

--- app/src/main/java/com/example/try10/MainActivity.kt
+++ app/src/main/java/com/example/try10/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.try10
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.try10.adapter.CategoryAdapter
+import com.example.try10.adapter.SectionSongListAdapter
 import com.example.try10.databinding.ActivityMainBinding
 import com.example.try10.models.CategoryModel
 import com.google.firebase.firestore.FirebaseFirestore
@@ -19,13 +28,37 @@ class MainActivity : AppCompatActivity() {
         binding =ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
         getCategories()
+        setupSection("secstion_1",binding.section1MainLayout,binding.secsion1Title,binding.section1RecyclerView)
+        setupSection("secstion_2",binding.section2MainLayout,binding.secsion2Title,binding.section2RecyclerView)
+        setupSection("secstion_3",binding.section3MainLayout,binding.secsion3Title,binding.section3RecyclerView)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        showPlayerView()
+    }
+    fun showPlayerView(){
+        binding.playerView.setOnClickListener(){
+            startActivity(Intent(this,PlayerActivity::class.java))
+        }
+        MyExoplayer.getCurrentSong()?.let {
+            binding.playerView.visibility=View.VISIBLE
+            binding.songTitleTextView.text=it.title
+            Glide.with(binding.songCoverImageView).load(it.coverUrl).apply(RequestOptions().transform(RoundedCorners(32))).into(binding.songCoverImageView)
+        }?:run{
+            binding.playerView.visibility=View.GONE
+        }
+    }
+    //Categories
     fun  getCategories(){
         FirebaseFirestore.getInstance().collection("category").get().addOnSuccessListener {
             val categoryList=it.toObjects(CategoryModel::class.java)
@@ -37,4 +70,21 @@ class MainActivity : AppCompatActivity() {
         binding.categoriesRecyclerView.layoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         binding.categoriesRecyclerView.adapter=categoryAdapter
     }
+    //Sections
+    fun setupSection(id:String,mainLayout:RelativeLayout,titleView:TextView,recyclerView: RecyclerView){
+        FirebaseFirestore.getInstance().collection("sections").document(id).get().addOnSuccessListener {
+            val section=it.toObject(CategoryModel::class.java)
+            section?.apply {
+                mainLayout.visibility=View.VISIBLE
+                titleView.text=name
+                recyclerView.layoutManager=LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+                recyclerView.adapter=SectionSongListAdapter(songs)
+                mainLayout.setOnClickListener{
+                    SongsListActivity.category=section
+                    startActivity(Intent(this@MainActivity,SongsListActivity::class.java))
+                }
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/try10/adapter/SectionSongListAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.try10.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.try10.MyExoplayer
+import com.example.try10.PlayerActivity
+import com.example.try10.SongsListActivity
+import com.example.try10.databinding.SelectionSongListRecyclerRowBinding
+import com.example.try10.databinding.SongListItemRecyclerRowBinding
+import com.example.try10.models.SongModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class SectionSongListAdapter(private val songIdList:List<String>):
+    RecyclerView.Adapter<SectionSongListAdapter.MyViewHolder>() {
+    class MyViewHolder(private val binding:SelectionSongListRecyclerRowBinding):RecyclerView.ViewHolder(binding.root){
+        fun bindingData(songId:String){
+            FirebaseFirestore.getInstance().collection("songs")
+                .document(songId).get()
+                .addOnSuccessListener {
+                    val song= it.toObject(SongModel::class.java)
+                    song?.apply {
+                        binding.songTitleTextView.text=title
+                        binding.songSubtitleTextView.text=subtitle
+                        Glide.with(binding.songCoverImageView).load(coverUrl).into(binding.songCoverImageView)
+                        binding.root.setOnClickListener{
+                            MyExoplayer.startPlaying(binding.root.context,song)
+                            it.context.startActivity(Intent(it.context,PlayerActivity::class.java))
+                        }
+                    }
+                }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding=SelectionSongListRecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return songIdList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindingData(songIdList[position])
+    }
+}
